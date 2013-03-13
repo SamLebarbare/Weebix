5dfb051b5412d763e567213fb18eb6def6d4bcd2
Samuel Loup <samuel.loup@gmail.com>
2013-03-13 17:45:33 +0100

    save purpose

---
+++ Weebix/Controllers/LobbyController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Weebix.Models;
+
+namespace Weebix.Controllers
+{
+    public class LobbyController : Controller
+    {
+        //
+        // GET: /Lobby/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
